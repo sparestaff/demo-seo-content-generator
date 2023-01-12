c6e916c316d0b9c879b9bedd56fbfc7e60ee39cf
hiroki0116 <hirokiseino0116@gmail.com>
2023-01-12 11:20:06 +1100

    Change font-family

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,18 @@
 import type { AppProps } from 'next/app'
+import Head from 'next/head';
 import "../styles/globals.css";
 import '../styles/custom-antd.css';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>SEO Content Generator</title>
+        <meta name="description" content="Developed by Safemode" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
