7c9500d7f3a68dcb09cdf2bc7f52703b5dabfaf7
hiroki0116 <hirokiseino0116@gmail.com>
2023-01-30 18:34:16 +1100

    fix

--- features/keyword/components/FAQ.tsx
+++ features/keyword/components/FAQ.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import Collapse from "antd/lib/collapse";
 import customParser from "utils/customParser";
-import { FAQ } from "types/FAQ";
+import type { FAQ } from "types/FAQ";
 
 const { Panel } = Collapse;
 
