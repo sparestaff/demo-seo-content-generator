fac379b7334535567ee12e7c9b0845a15197cd86
hiroki0116 <hirokiseino0116@gmail.com>
2023-01-30 11:26:06 +1100

    Add script

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,46 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        {/* <!-- Start of Freshwork Embed Code -->  */}
+        <script
+          async
+          src="//au.fw-cdn.com/20165476/88805.js"
+          data-chat="false"
+        />
+        {/* <!-- End of Freshwork Embed Code --> */}
+        {/* <!-- Google Tag Manager --> */}
+        {/* eslint-disable-next-line @next/next/next-script-for-ga */}
+        <script
+          async
+          dangerouslySetInnerHTML={{
+            __html: `
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-57GLT3J');
+            `,
+          }}
+        />
+        {/* <!-- End Google Tag Manager --> */}
+      </Head>
       <body>
+        {/* <!-- Google Tag Manager (noscript) --> */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-57GLT3J"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+        {/* <!-- End Google Tag Manager (noscript) --> */}
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
