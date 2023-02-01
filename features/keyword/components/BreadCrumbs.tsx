5c1ec7e344d314b43250eda4db083e04975afbda
hiroki0116 <hirokiseino0116@gmail.com>
2023-02-01 15:41:15 +1100

    Add region

--- features/keyword/components/BreadCrumbs.tsx
+++ features/keyword/components/BreadCrumbs.tsx
@@ -1,17 +1,20 @@
 import Link from "next/link";
-import { camelCaseAll } from "utils/formatter";
+import { camelCaseAll, capitalizeFirstLetter } from "utils/formatter";
 const BreadCrumbs = ({
   keyword,
   location,
+  region,
 }: {
   keyword: string | undefined;
   location?: string | undefined;
+  region?: string;
 }) => {
   return (
     <div className="text-center mt-5 mb-10 px-5 sm:px-5 md:px-5 lg:px-5 xl:px-0">
       You are here: <Link href="https://safemode.com.au/">Home</Link> /{" "}
       {keyword}
-      {location ? ` / ${camelCaseAll(location)}` : null}
+      {region ? ` / ${camelCaseAll(region)}` : null}
+      {location ? ` / ${capitalizeFirstLetter(location)}` : null}
     </div>
   );
 };
